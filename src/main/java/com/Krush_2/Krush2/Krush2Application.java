205c0d036adb22005d7f3b03ccc14182e36a0d29
BuildTools <unconfigured@null.spigotmc.org>
2024-07-03 16:57:16 +0900

    Feat: Spring Init

---
+++ src/main/java/com/Krush_2/Krush2/Krush2Application.java
@@ -0,0 +1,13 @@
+package com.Krush_2.Krush2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Krush2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Krush2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Krush_2/Krush2/Krush2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Krush_2.Krush2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Krush2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
