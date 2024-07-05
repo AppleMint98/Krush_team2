c7c9caf6973331808576a698c71894f027f8e892
AppleMint98 <hong721816@konkuk.ac.kr>
2024-07-06 05:10:27 +0900

    :엔티티수정

--- src/main/java/com/Krush_2/Krush2/service/GoalService.java
+++ src/main/java/com/Krush_2/Krush2/service/GoalService.java
@@ -6,39 +6,49 @@ import com.Krush_2.Krush2.dto.GoalDto;
 import com.Krush_2.Krush2.exception.CustomException;
 import com.Krush_2.Krush2.repository.GoalRepository;
 import com.Krush_2.Krush2.repository.SubGoalRepository;
-import com.Krush_2.Krush2.response.ExceptionResponseStatus;
+import com.Krush_2.Krush2.response.status.ExceptionResponseStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class GoalService {
 
-  private final GoalRepository goalRepository;
-  private final SubGoalRepository subGoalRepository;
+    private final GoalRepository goalRepository;
+    private final SubGoalRepository subGoalRepository;
 
-  public void register(GoalDto request) {
-    Goal savedGoal = goalRepository.save(Goal.builder()
-      .startAt(request.getStartAt())
-      .endAt(request.getEndAt())
-      .contents(request.getContents())
-      .build());
-    request.getSubGoalList().forEach(x -> {
-        subGoalRepository.save(SubGoal.builder()
-          .goal(savedGoal)
-          .contents(x.getContents())
-          .emoji(x.getEmoji())
-          .build());
-      }
-    );
-  }
+    public void register(GoalDto request) {
+        Goal savedGoal = goalRepository.save(Goal.builder()
+                .startAt(request.getStartAt())
+                .endAt(request.getEndAt())
+                .contents(request.getContents())
+                .build());
+        request.getSubGoalList().forEach(x -> {
+                    subGoalRepository.save(SubGoal.builder()
+                            .goal(savedGoal)
+                            .contents(x.getContents())
+                            .emoji(x.getEmoji())
+                            .build());
+                }
+        );
+    }
 
-  public GoalDto getInfo(long goalId) {
-    Goal goal = goalRepository.findById(goalId)
-      .orElseThrow(() -> new CustomException(ExceptionResponseStatus.GOAL_NOT_FOUND));
-    List<SubGoal> subGoalList = subGoalRepository.findAllByGoal(goal);
-    return GoalDto.from(goal, subGoalList);
-  }
+    public GoalDto getInfo(long goalId) {
+        Goal goal = goalRepository.findById(goalId)
+                .orElseThrow(() -> new CustomException(ExceptionResponseStatus.GOAL_NOT_FOUND));
+        List<SubGoal> subGoalList = subGoalRepository.findAllByGoal(goal);
+        return GoalDto.from(goal, subGoalList);
+    }
+
+    public List<GoalDto> getPastGoals() {
+        List<Goal> pastGoals = goalRepository.findByEndAtBefore(LocalDate.now());
+        return pastGoals.stream().map(goal -> {
+            List<SubGoal> subGoals = subGoalRepository.findAllByGoal(goal);
+            return GoalDto.from(goal, subGoals);
+        }).collect(Collectors.toList());
+    }
 }
