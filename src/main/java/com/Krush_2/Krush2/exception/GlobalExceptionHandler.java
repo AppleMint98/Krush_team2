187c9df07f5ad19634d639bf596eeaf138f6208a
sh1220 <jiu0009@naver.com>
2024-07-06 03:51:52 +0900

    fix: Exception Handler

--- src/main/java/com/Krush_2/Krush2/exception/GlobalExceptionHandler.java
+++ src/main/java/com/Krush_2/Krush2/exception/GlobalExceptionHandler.java
@@ -1,19 +1,22 @@
 package com.Krush_2.Krush2.exception;
 
-import com.Krush_2.Krush2.dto.ErrorResponse;
+import com.Krush_2.Krush2.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
+@Order(Ordered.LOWEST_PRECEDENCE)
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(CustomException.class)
   public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
     log.error("{}", e.getMessage());
-    ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode(), e.getMessage());
-    return ResponseEntity.status(e.getErrorCode().getStatus()).body(errorResponse);
+    ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode().getStatus(), e.getMessage());
+    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
   }
 }
