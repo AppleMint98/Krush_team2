35675a0cbcce3ddbff20509c152e04445acec5ea
AppleMint98 <hong721816@konkuk.ac.kr>
2024-07-05 20:45:22 +0900

    DB구축

---
+++ src/main/java/com/Krush_2/Krush2/domain/Alarm.java
@@ -0,0 +1,23 @@
+package com.Krush_2.Krush2.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "alarm")
+public class Alarm {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "alarm_id")
+    private Long alarmId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "diary_id", nullable = false)
+    private Diary diary;
+
+    @Column(name = "day", nullable = false)
+    private Integer day;
+
+    @Column(name = "Field", length = 255)
+    private String field;
+}
